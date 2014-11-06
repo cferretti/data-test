633416031dab0251c6b610681dd4f389fabffb85
Cédric Ferretti <ferretticedric@gmail.com>
2014-11-06 16:33:46 +0800

    Change citizen me

--- citizenme.js
+++ citizenme.js
@@ -1,37 +1,53 @@
 $(document).ready(function(){
+	var urlDataTotal = 'https://rawgit.com/cferretti/data-test/master/%s-total.json';
+	var offsetService = 4;
+	var offsetVoteType = 3;
+	var down_vote = 'unreasonablechange'; 
+	var up_vote = 'reasonablechange';
+	var display_null = true;
 
 	var tableGenerator = {
 		elem : null,
-		services : [],
+		services : [
+			{
+				_id : 'facebook',
+				text : 'Facebook'
+			},
+			{
+				_id : 'twitter',
+				text : 'Twitter'
+			},
+			{
+				_id : 'flickr',
+				text : 'Flickr'
+			},
+			{
+				_id : 'google',
+				text : 'Google'
+			},
+			{
+				_id : 'linkedin',
+				text : 'LinkedIn'
+			},
+			{
+				_id : 'instagram',
+				text : 'Instagram'
+			},
+
+		],
+		data : [],
 		datatable : null,
 		init : function(elem){
 			this.elem = $(elem);
-			var _self = this;
-			this.getData(function(data){
-				_self.getServices(data);
-				_self.setData(data);
-			});
+			this.initTable();
+			this.createDropdown();
 		},
-		setData : function(data){
-			console.log(data);
-			console.log(this);
-			this.datatable = this.elem.dataTable({
-				data: data,
-				bFilter : true,
-				sDom : 'rtpli',
-		        columns: [
-		            { "title": "Service" },
-		            { "title": "Version", "class": "center" },
-		            { "title": "Avg. Note" }
-		        ]
-			});
-
-			//Create the dropdown menu
+		createDropdown : function(){
 			var dropdown = $('<select></select>');
 			for(i in this.services){
 				var option = $('<option></option>').attr(
-						{'value' : this.services[i]	}
-					).text(this.services[i]);
+						{'value' : this.services[i]._id	}
+					).text(this.services[i].text);
 
 				dropdown.append(option);
 			}
@@ -39,34 +55,97 @@ $(document).ready(function(){
 			var _self = this;
 			//add listener to dropdown
 			dropdown.on('change', function _changeServiceListener(event){
-				_self.changeServiceEvent(this)
+				_self.getDataTotal(this, function(data){ _self.createData(data) });
 			});
 
 			dropdown.trigger('change');
-
 			this.elem.before(dropdown);
 		},
-		getData : function(callback){
-			console.log('ok');
-			$.ajax({
-				url : 'https://rawgit.com/cferretti/data-test/master/getAWSData.json',
-				success : function(data){
-					callback(data);
-				}
-			})
-		},
-		getServices : function(data){
+		createData : function(data){
+			var converted_data = [];
+			this.data = [];
+			var can_be_displayed = false;
 			for(i in data){
-				//Makeit unik
-				if(this.services.indexOf(data[i][0]) === -1){
-					this.services.push(data[i][0]);
+				can_be_displayed = true;
+				//Get part of the string
+				var parts = data[i].name.split('/');
+				var service = parts[offsetService];
+				var type = parts[offsetVoteType];
+				//Check if revision exit
+				var subparts = service.split('-');
+				var revision = '';
+				if(typeof(subparts[2]) !== 'undefined' && subparts[2] !== ''){
+					revision = subparts[2];
+				}else if(!display_null){
+					can_be_displayed = false;
 				}
+
+				if(typeof(subparts[1]) !== 'undefined'){
+					service = subparts[0];
+				}
+
+				var reasonable_vote = 0;
+				var unreasonable_vote = 0;
+
+				if(type == down_vote){
+					unreasonable_vote = data[i].count;
+				}else if(type == up_vote){
+					reasonable_vote = data[i].count;
+				}
+
+				if(can_be_displayed){
+					if(converted_data.indexOf(parts[offsetService]) === -1){
+						converted_data[parts[offsetService]] = { 
+							'service' : service,
+							'rev' : revision,
+							'unreasonable' : unreasonable_vote,
+							'reasonable' : reasonable_vote,
+							'time' : new Date(data[i].start_time*1000).toLocaleDateString()
+						};
+					}else{
+						if(type == down_vote){
+							converted_data[parts[offsetService]].unreasonable = unreasonable_vote;
+						}else if(type == up_vote){
+							converted_data[parts[offsetService]].reasonable = reasonable_vote;
+						}
+					}
+				}
+
 			}
 
-			console.log(this.services);
+			for(i in converted_data){
+				this.data.push(converted_data[i]);
+			}
+
+			this.setData();
 		},
-		changeServiceEvent : function(select){
-			this.datatable.fnFilter($(select).val());
+		initTable : function(){
+			this.datatable = this.elem.dataTable({
+				data: this.data,
+				sDom : 'rtpli',
+		        columns: [
+		            { "title": "Service", "data" : "service" },
+		            { "title": "Revision", "class": "center", "data" : "rev" },
+		            { "title": "Reasonable", "data" : "reasonable" },
+		            { "title": "Unreasonable", "data" :"unreasonable"},
+		            { "title": "Avg. Note", "data" : "time" }
+		        ],
+		        order: [[ 4, "desc" ]]
+			});
+		},
+		setData : function(){
+			this.datatable.fnClearTable();
+	        this.datatable.fnAddData(this.data);
+	        this.datatable.fnAdjustColumnSizing();
+		},
+		getDataTotal : function(select, callback){
+			$.ajax({
+				url : urlDataTotal.replace('%s',$(select).val()),
+				success : function(data){
+					console.log(data);
+					callback(data);
+				}
+			});
 		}
 	};
 
