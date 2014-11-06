4f002c29fb6bc15abcd18bccfe1ed4eaacdbfd39
Cédric Ferretti <ferretticedric@gmail.com>
2014-11-06 19:33:19 +0800

    Add sub level for test

--- citizenme.js
+++ citizenme.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	var urlDataTotal = 'https://rawgit.com/cferretti/data-test/master/%s-total.json';
+	var urlDataPoint = 'https://rawgit.com/cferretti/data-test/master/%s-points-total.json';
 	var offsetService = 4;
 	var offsetVoteType = 3;
 	var down_vote = 'unreasonablechange'; 
@@ -46,7 +47,7 @@ $(document).ready(function(){
 			var dropdown = $('<select></select>');
 			for(i in this.services){
 				var option = $('<option></option>').attr(
-						{'value' : this.services[i]._id	}
+						{'value' : this.services[i]._id, id:'dropdown_services'}
 					).text(this.services[i].text);
 
 				dropdown.append(option);
@@ -55,11 +56,29 @@ $(document).ready(function(){
 			var _self = this;
 			//add listener to dropdown
 			dropdown.on('change', function _changeServiceListener(event){
-				_self.getDataTotal(this, function(data){ _self.createData(data) });
+				_self.getDataTotal(this, function(data){ _self.createData(data); });
 			});
 
 			dropdown.trigger('change');
 			this.elem.before(dropdown);
+		},
+		createBackBtn : function(){
+			if($('#btn_back').length > 0){
+				$('#btn_back').remove();
+			}
+
+			var btn_back = $('<button></button>');
+			btn_back.attr({
+				id : 'btn_back'
+			}).text('Back');
+
+			btn_back.on('click', function(e){
+				e.preventDefault();
+				$('#dropdown_services').trigger('change');
+				this.remove();
+			});
+
+			this.elem.after(btn_back);
 		},
 		createData : function(data){
 			var converted_data = [];
@@ -74,8 +93,8 @@ $(document).ready(function(){
 				//Check if revision exit
 				var subparts = service.split('-');
 				var revision = '';
-				if(typeof(subparts[1]) !== 'undefined' && subparts[1] !== ''){
-					revision = subparts[1];
+				if(typeof(subparts[2]) !== 'undefined' && subparts[2] !== ''){
+					revision = subparts[2];
 				}else if(!display_null){
 					can_be_displayed = false;
 				}
@@ -122,18 +141,90 @@ $(document).ready(function(){
 
 			this.setData();
 		},
+		createDataPoint : function(data, rev){
+			var converted_data = [];
+			this.data = [];
+			var can_be_displayed = false;
+			for(i in data){
+				can_be_displayed = true;
+				//Get part of the string
+				var parts = data[i].name.split('/');
+				var service = parts[offsetService+1];
+				var type = parts[offsetVoteType];
+				//Check if revision exit
+				var subparts = service.split('-');
+				var revision = '';
+				if(typeof(subparts[1]) !== 'undefined' && subparts[1] !== ''){
+					revision = subparts[1];
+				}else if(!display_null){
+					can_be_displayed = false;
+				}
+
+				if(typeof(subparts[1]) !== 'undefined'){
+					service = subparts[0];
+				}
+
+				var reasonable_vote = 0;
+				var unreasonable_vote = 0;
+
+				if(type == down_vote){
+					unreasonable_vote = data[i].count;
+				}else if(type == up_vote){
+					reasonable_vote = data[i].count;
+				}
+
+				if(subparts[2] !== rev){
+					can_be_displayed = false;
+				}
+
+				var index = service+revision;
+
+				if(can_be_displayed){
+					if(converted_data.indexOf(index) === -1){
+						var date = new Date(data[i].start_time*1000);
+						converted_data[index] = { 
+							'service' : service,
+							'rev' : revision,
+							'unreasonable' : unreasonable_vote,
+							'reasonable' : reasonable_vote,
+							'time' : date.getUTCFullYear()+"/"+(date.getUTCMonth() + 1)+"/"+date.getUTCDate()
+						};
+					}else{
+						if(type == down_vote){
+							converted_data[index].unreasonable = unreasonable_vote;
+						}else if(type == up_vote){
+							converted_data[index].reasonable = reasonable_vote;
+						}
+					}
+				}
+
+			}
+
+			for(i in converted_data){
+				this.data.push(converted_data[i]);
+			}
+
+			this.setData();
+		},
 		initTable : function(){
 			this.datatable = this.elem.dataTable({
 				data: this.data,
 				sDom : 'rtpli',
 		        columns: [
-		            { "title": "Service", "data" : "service" },
-		            { "title": "Revision", "class": "center", "data" : "rev" },
+		            { "title": "Service", "class":"service", "data" : "service" },
+		            { "title": "Revision", "class": "rev", "data" : "rev" },
 		            { "title": "Reasonable", "data" : "reasonable" },
 		            { "title": "Unreasonable", "data" :"unreasonable"},
 		            { "title": "Avg. Note", "data" : "time" }
 		        ],
 		        order: [[ 4, "desc" ]]
+			});
+
+			var _self = this;
+			this.elem.find('tbody').on( 'click', 'tr', function () {
+				var rev = $(this).find('.rev').text()
+				var service = $(this).find('.service').text()
+				_self.getDataPoint(rev, service, function(data){ _self.createDataPoint(data, rev); _self.createBackBtn(); });
 			});
 		},
 		setData : function(){
@@ -149,49 +240,19 @@ $(document).ready(function(){
 					callback(data);
 				}
 			});
+		},
+		getDataPoint : function(revision, service, callback){
+			$.ajax({
+				url : urlDataPoint.replace('%s',service),
+				success : function(data){
+					console.log(data);
+					callback(data);
+				}
+			});
 		}
 	};
 
-	var Chart = {
-		elem : null,
-		lines : [],
-		generate : function(elem){
-			this.elem = elem;
-			var svg = d3.select(this.elem)
-			.append("svg")
-			.attr('width', 800)
-			.attr('height', 200)
-			.style('background','red');
 
-			this.lines = svg.append('g').attr('class', 'lines');
-			//make the line
-			this.createLines();
-			//Make the row
-
-			//Place point
-		},
-		createLines : function(){
-			this.lines
-			.selectAll('line')
-			.data([0,50,1,20])
-			.enter()
-			.append('line')
-			.attr('class','line')
-			.style('fill','black')
-			.attr('x',function(d){
-				return d;
-			})
-			.attr('y', function(d){
-				return d;
-			});
-		},
-		createRows : function(){
-
-		},
-		placePoints : function(){
-
-		}
-	}
 	$.fn.citizenTable = function(options) {  
 		var config = {}; 
 		var defaults = $.extend(defaults, options);
@@ -203,16 +264,5 @@ $(document).ready(function(){
 		return this;  
 	}; 
 
-	$.fn.citizenGraphic = function(options){
-		var config = {};
-		var defaults = $.extend(defaults, options);
-
-		this.each(function() {
-			Chart.generate(this);
-		});
-
-		return this;
-	}
 	$('#test').citizenTable();
-	$('#graphics').citizenGraphic();
 });
